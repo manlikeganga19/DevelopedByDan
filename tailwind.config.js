f4675bb2f7d5638ab28d5dadd1879ae36119150d
Daniel <owenjodaniel19@gmail.com>
2023-08-09 19:36:04 +0300

    configured tailwind and added some module exports

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      primary: "Saira",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1.5rem",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        primary: "#23262F",
+        accent: "#286F6C",
+        grey: {
+          DEFAULT: "#919297",
+          1: "#D9D9D9",
+          2: "#E7E9EB",
+          3: "#F5F5F5",
+        },
+        white: "#fff",
+        pink: "pink",
+      },
+    },
+  },
+  plugins: [],
+};
+
